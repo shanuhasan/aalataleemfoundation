21839504652ed511833b34c183ff2241d5e3fa97
Shahnawaz <shahnawaz@ranoliaventures.com>
2025-09-04 14:10:54 +0530

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,80 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <meta charset="utf-8">
+    <title>@yield('title') | Aala Taleem Foundation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Favicon -->
+    {{-- <link href="{{ asset('assets/frontend/images/musheeda.png') }}" rel="icon"> --}}
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <!-- Libraries CSS -->
+    <link href="{{ asset('assets/frontend/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/magnific-popup.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/themify-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/nice-select.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/flaticon.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/gijgo.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/slicknav.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/style.css') }}" rel="stylesheet">
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body>
+
+    @include('layouts.navbar')
+
+    @yield('content')
+
+    @include('layouts.footer')
+
+    <a href="#" class="back-to-top"><i class="ion-ios-arrow-up"></i></a>
+
+    <!-- Libraries JS -->
+    <script src="{{ asset('assets/frontend/js/vendor/modernizr-3.5.0.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/vendor/jquery-1.12.4.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/ajax-form.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/scrollIt.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.scrollUp.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/nice-select.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.slicknav.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/plugins.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/gijgo.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/contact.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.form.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/mail-script.js') }}"></script>
+
+    <script src="{{ asset('assets/frontend/js/main.js') }}"></script>
+
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\frontend\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +20,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
